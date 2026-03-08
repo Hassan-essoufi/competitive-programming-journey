328128c3b96b4b1701a7c12901c13a41bb304b2c
hassan <hassan>
2026-03-08 17:31:43 +0000

    fast api

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+from typing import List
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
-
-@app.get("/")
-def valide():
-    return {"api running": "we run api"}
-
+@app.post("/files/")
+async def create_file(file: bytes = 
+File(...)):
+    return {"file_size": len(file)}
+@app.post("/uploadfiles/")
+async def create_upload_files(files: 
+List[UploadFile] = File(...)):
+    return {"filenames": [file.filename 
+for file in files]}
