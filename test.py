ee843ac25bfe825a0d8773d8b95450c77e6b9b67
Hassan-essoufi <hassanessoufi2004@gmail.com>
2026-04-05 18:38:02 +0100

    two pointers

--- test.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-
-async def hello():
-    print('Hello')
-    await asyncio.sleep(1)  # Wait for one second before printing 'world'
-    print('World')
-
-# Run the coroutine in an event loop
-asyncio.run(hello())
